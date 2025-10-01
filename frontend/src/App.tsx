eb0a56f17b8b9b14244c64eeb2b7127c151a8fbe
Rich182182 <zhenu1276@gmail.com>
2025-10-01 16:10:36 +0300

    Setup React frontend with TypeScript

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Exoplanet Detector</h1>
+      <p>Загрузка файлов FITS...</p>
+    </div>
+  );
+}
+
+export default App;
